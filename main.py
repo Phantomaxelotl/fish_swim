675932694a3b717df0ffb2d95467b94c5fe92593
Phantomaxelotl <86864307+Phantomaxelotl@users.noreply.github.com>
2022-07-25 23:00:04 +0000

    ver 1

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import random
 pygame.init
+
 screen_info=pygame.display.info()
+
 size=(width,height)=(int(screen_info.current_w),int(screen_info.current_h))
+
 screen=pygame.display.set_model(size)
+
 clock=pygame.time.clock()
+
+speed=pygame.math.vector2((0,10))
+rotate=random.randint(0,360)
+speed.rotate_ip(rotate)
+fish_image=pygame.transform.rotate(fish_image,180_rotate)
+
+def move_fish():
+global speed
+fishrect.move_ip(speed)
+
+if fish_rect.left<0 or fish_rect.right>screen_info.current w:
+speed[0]*=-1
+fish_image=pygame.transform.flip(fish_image,True,False)
+fish_rect.move_ip(speed[0],0)
 
 def main():
   while True:

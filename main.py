14e9d652eff92b35a5412f1655a7848a6ce48ac6
Phantomaxelotl <86864307+Phantomaxelotl@users.noreply.github.com>
2022-07-26 20:52:36 +0000

    finished* non functional

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+import pygame
+import sys
+from pygame.locals import*
 pygame.init
 
 screen_info=pygame.display.info()
@@ -9,23 +12,44 @@ screen=pygame.display.set_model(size)
 
 clock=pygame.time.clock()
 
+
+
+fish_image=pygame.image.load("fish.png")
+fish_image=pygame.transform.smoothscale(fish_image(80,80))
+fish_rect=fish_image.get_rect()
+fish_rect.center=(width//2,height//2)
+
+
+speed.rotate_ip(rotate)
+fish_image=pygame.transform.rotate(fish_image,180-rotate)
+
 speed=pygame.math.vector2((0,10))
 rotate=random.randint(0,360)
 speed.rotate_ip(rotate)
-fish_image=pygame.transform.rotate(fish_image,180_rotate)
+fish_image=pygame.transform.rotate(fish_image,180-rotate)
 
 def move_fish():
-global speed
-fishrect.move_ip(speed)
+  global speed, fish_image
+  fishrect.move_ip(speed)
 
-if fish_rect.left<0 or fish_rect.right>screen_info.current w:
-speed[0]*=-1
-fish_image=pygame.transform.flip(fish_image,True,False)
-fish_rect.move_ip(speed[0],0)
+  screen_info=pygame.display.Info()
+  fish_rect.move_ip(speed)
+  
+  if fish_rect.left<0 or fish_rect.right>screen_info.current_w:
+    speed[0]*=-1
+    fish_image=pygame.transform.flip(fish_image,True,False)
+    fish_rect.move_ip(speed[0],0)
+  
+  def main():
+    while True:
+      clock.tick(60) #refresh game page every second
+      for event in pygame.event.get():
+        if event.type == QUIT:
+          sys.exit()
 
-def main():
-  while True:
-    clock.tick(60)
-    screen.fill(65,190,220)
-    screen.blit(fish_image)
-    pygame.display.flip()
+      
+      screen.fill(65,190,220)
+      screen.blit(fish_image) #blit display fish
+      pygame.display.flip()
+  if __nmae__ == "__main__":
+    main()
